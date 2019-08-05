14438e58370e75adc17ecbb9c23e64fa0f455d20
Ben Freir <37246948+CatAndDogSoup@users.noreply.github.com>
2019-08-05 19:39:41 +0100

    Update SeamlessFKIK.py

--- SeamlessFKIK.py
+++ SeamlessFKIK.py
@@ -38,6 +38,6 @@ else:
 		arm01Vec = [mc.xform('JNT_Left_FK_Elbow', t=1, ws=1, q=1)[i] - mc.xform('CTRL_Left_FK_Shoulder', t=1, ws=1, q=1)[i] for i in range(3)]
 		arm02Vec = [mc.xform('JNT_Left_FK_Elbow', t=1, ws=1, q=1)[i] - mc.xform('CTRL_Left_FK_Wrist', t=1, ws=1, q=1)[i] for i in range(3)]
 
-		mc.xform('CTRL_Left_Arm_IK_PoleVector', t=[mc.xform('CTRL_Left_FK_Elbow', t=1, q=1, ws=1)[i] + arm01Vec[i] * .75 + arm02Vec[i] * .75 for i in range(3)], ws=1)
+		#mc.xform('CTRL_Left_Arm_IK_PoleVector', t=[mc.xform('CTRL_Left_FK_Elbow', t=1, q=1, ws=1)[i] + arm01Vec[i] * .75 + arm02Vec[i] * .75 for i in range(3)], ws=1)
 
 		mc.setAttr('CTRL_Left_FKIK.IK', 1)
