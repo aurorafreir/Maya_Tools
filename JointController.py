f3fbf231e25e4abc66874d81c80021bad0183a8d
Ben Freir <37246948+CatAndDogSoup@users.noreply.github.com>
2019-09-14 20:24:56 +0100

    added for-each loop
    added array for selected joints and ran through each to make controllers at each joint pointing at the next

--- JointController.py
+++ JointController.py
@@ -1,18 +1,25 @@
 import maya.cmds as cmds
 
-tempSel_Parent = cmds.ls(sl=True,long=True)
-cmds.pickWalk( direction='down' );
-tempSel_AimAt = cmds.ls(sl=True,long=True)
-cmds.select( d=True );
+tempSel_JointArray = cmds.ls(sl=True)
+print tempSel_JointArray
 
-cmds.circle(name='CTRL_');
-cmds.rotate(0,90,0);
-cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
-cmds.bakePartialHistory()
-cmds.group( em=True, name='PIVOT_');
-cmds.parent('CTRL_', 'PIVOT_');
+for i in tempSel_JointArray:
+    cmds.select(i)
+    tempSel_Parent = cmds.ls(sl=True)
+    print tempSel_Parent
+    cmds.pickWalk( direction='down' );
+    tempSel_AimAt = cmds.ls(sl=True)
+    print tempSel_AimAt
+    
+    cmds.select( d=True );
+    cmds.circle(name='CTRL_' + (i));
+    cmds.rotate(0,90,0);
+    cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
+    cmds.bakePartialHistory()
+    cmds.group( em=True, name='PIVOT_' + (i));
+    cmds.parent('CTRL_' + (i), 'PIVOT_' + (i));
 
-cmds.parentConstraint( tempSel_Parent, 'PIVOT_', mo=False, name='tempParentConstraint');
-cmds.delete( 'tempParentConstraint');
-cmds.aimConstraint( tempSel_AimAt, 'PIVOT_', name='tempAimConstraint');
-cmds.delete( 'tempAimConstraint');
+    cmds.parentConstraint( tempSel_Parent, 'PIVOT_' + (i) , mo=False, name='tempParentConstraint' +(i));
+    cmds.delete( 'tempParentConstraint' +(i));
+    cmds.aimConstraint( tempSel_AimAt, 'PIVOT_' +(i), name='tempAimConstraint' +(i));
+    cmds.delete( 'tempAimConstraint' +(i));
