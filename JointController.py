36cb773b3eb9bda4726c35fd91d0db5ecd050b7f
Ben Freir <37246948+CatAndDogSoup@users.noreply.github.com>
2019-09-15 22:16:56 +0100

    cleaned up code EVEN MORE

--- JointController.py
+++ JointController.py
@@ -1,39 +1,39 @@
-#makes a square NURBS controller and parent constraints the joints to the controllers
+# Makes a square NURBS controller and parent constraints the joints to the controllers
 import maya.cmds as cmds
 
-#makes an array of the selected joints
-tempSel_JointArray = cmds.ls( type=('joint'), sl=True)
+# Makes an array of the selected joints
+tempSel_jointArray = cmds.ls( type=('joint'), sl=True)
 
-for i in tempSel_JointArray:
+for i in tempSel_jointArray:
     
-    #selects current joint and sets it as variable tempSel_Parent
+    # Selects current joint and sets it as variable tempSel_Parent
     cmds.select(i)
-    tempSel_Parent = cmds.ls( sl=True)
+    tempSel_parent = cmds.ls( sl=True)
     
-    #selects child joint and sets it as variable tempSel_AimAt
-    tempSel_AimAt = cmds.listRelatives( type='joint')
+    # Selects child joint and sets it as variable tempSel_AimAt
+    tempSel_aimAt = cmds.listRelatives( type='joint')
     
-    #creates square NURBS curve and deletes it's history
+    # Creates square NURBS curve and deletes it's history
     cmds.select( d=True );
-    cmds.curve( d=1, p=[(-0.5, 0, .5), (-0.5, 0, -.5), (.5, 0, -.5), (.5, 0, .5), (-0.5, 0, .5)], name='CTRL_' + (i));
+    cmds.curve( d=1, p=[(-0.5, 0, .5), (-0.5, 0, -.5), (.5, 0, -.5), (.5, 0, .5), (-0.5, 0, .5)], name='CTRL_' + i);
     cmds.rotate( 0,0,90);
     cmds.makeIdentity( apply=True, t=1, r=1, s=1, n=0)
     cmds.bakePartialHistory()
     
-    #makes a group and makes the controller a child of the PIVOT group
+    # Makes a group and makes the controller a child of the PIVOT group
     cmds.group( em=True, name='PIVOT_' + (i));
     cmds.parent( 'CTRL_' + (i), 'PIVOT_' + (i));
     
-    #makes parent constraint for controller location
-    cmds.parentConstraint( tempSel_Parent, 'PIVOT_' + (i) , mo=False, name='tempParentConstraint' + (i));
+    # Makes parent constraint for controller location
+    cmds.parentConstraint( tempSel_parent, 'PIVOT_' + i , mo=False, name='tempParentConstraint' + i);
     cmds.delete( 'tempParentConstraint' + (i));
     
-    #makes aim constraint for controller orientation
-    cmds.aimConstraint( tempSel_AimAt, 'PIVOT_' + (i), name='tempAimConstraint' + (i));
+    # Makes aim constraint for controller orientation
+    cmds.aimConstraint( tempSel_aimAt, 'PIVOT_' + i, name='tempAimConstraint' + i);
     cmds.delete( 'tempAimConstraint' + (i));
     
-    #parent constrains the joint to the controller
-    cmds.select( 'CTRL_' + (i))
+    # Parent constrains the joint to the controller
+    cmds.select( 'CTRL_' + i)
     cmds.select( (i), add=True)
-    cmds.parentConstraint( name='parentConstraint_' + (i) + '_CTRL_' + (i))
+    cmds.parentConstraint( name='parentConstraint_' + (i) + '_CTRL_' + i)
     cmds.select( d=True)
