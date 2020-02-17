a5271eaec92faff4fc0b43c91302f4c6f36ef7b5
Aurora Freir <37246948+CatAndDogSoup@users.noreply.github.com>
2020-02-17 12:26:08 +0000

    add check for array size, and specific names for each locator
    check if array size is only two (2) objects
    makes a locator for the first item, and names it "wireLocator1", does the same for the second object but names it "wireLocator2"
    have also started adding in a bit of code to make the curve and then delete the locators at the end

--- AurString.py
+++ AurString.py
@@ -2,11 +2,26 @@ import maya.cmds as cmds
 
 selected = cmds.ls(selection=True)
 
-for item in selected:
-    rPiv = cmds.xform(item, q=True, rp=True)  # query rotation pivot
-    sPiv = cmds.xform(item, q=True, sp=True)  # query scale pivot
+selected_len = (len(selected))
+print selected_len
 
-    loc = cmds.spaceLocator(n=item + "wireLocator")
-    cmds.xform(t=rPiv)
+if len(selected) == 2:
+    for item in selected[:1]:
+        rPiv = cmds.xform(item, q=True, rp=True)  # query rotation pivot
+        sPiv = cmds.xform(item, q=True, sp=True)  # query scale pivot
 
-cmds.curve()
+        loc = cmds.spaceLocator(n="wireLocator1")
+        cmds.xform(t=rPiv)
+
+    for item in selected[1:]:
+        rPiv = cmds.xform(item, q=True, rp=True)  # query rotation pivot
+        sPiv = cmds.xform(item, q=True, sp=True)  # query scale pivot
+
+        loc = cmds.spaceLocator(n= "wireLocator2")
+        cmds.xform(t=rPiv)
+
+
+#cmds.curve('string_##',)
+
+#cmds.delete("wireLocator1")
+#cmds.delete("wireLocator2")
