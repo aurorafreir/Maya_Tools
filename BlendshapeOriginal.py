7a59786772096d2af5c2bf3aa8e48ba875390333
Aurora Freir <CatAndDogSoup@gmail.com>
2020-03-02 11:54:03 +0000

    added setAttr for intermediate, and clean up code
    Script current duplicates object, turns off Intermediate Object in the ShapeOrig node, and deletes the non-origin node
    
    todo: reapply texture, and unlock attrs

--- BlendshapeOriginal.py
+++ BlendshapeOriginal.py
@@ -3,12 +3,16 @@ import maya.cmds as cmds
 SelectedObj = cmds.ls(sl=True, sn=True)
 
 if SelectedObj:
-    print SelectedObj
-    cmds.duplicate(n="Blendshape")
-    cmds.select("{}Orig".format(SelectedObj))
+    cmds.duplicate(n="{}Blendshape".format(SelectedObj[0]))
+    cmds.setAttr("{}BlendshapeShapeOrig.intermediateObject".format(SelectedObj[0]), 0)
+    cmds.delete("{}BlendshapeShape".format(SelectedObj[0]))
+
+#TODO reapply material
+
+#TODO unlock translate attrs
 
 #axis = ['X', 'Y', 'Z']
 #attrs = ['t', 'r', 's']
 #for ax in axis:
-    #for attr in attrs:
-        #cmds.setAttr("Blendshape"+'.'+attr+ax, lock=0)
+#    for attr in attrs:
+#       cmds.setAttr("Blendshape"+'.'+attr+ax, lock=0)
