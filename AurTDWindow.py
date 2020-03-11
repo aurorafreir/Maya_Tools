71a6589d1ef91b89f94a383db4028b2f1b2c339c
Aurora Freir <CatAndDogSoup@gmail.com>
2020-03-11 17:21:23 +0000

    added in SingleFramePlayblast script

--- AurTDWindow.py
+++ AurTDWindow.py
@@ -1,6 +1,7 @@
 import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
 import maya.OpenMayaUI as OpenMayaUI
+import os
 
 winID = 'aurWindow'
 
@@ -179,16 +180,67 @@ def AurTDOverscan(self):
 def AurTDSingleFramePlayblast(self):
 	#gets current frame
 	CurrentFrame = cmds.currentTime(q=True)
+
 	#gets current render format, and sets image format to jpg
 	CurrentImageFormat = cmds.getAttr("defaultRenderGlobals.imageFormat")
 	cmds.setAttr("defaultRenderGlobals.imageFormat", 8) # *.jpg
+
 	#gets current render resolution
 	RenderWidth = cmds.getAttr("defaultResolution.width")
 	RenderHeight= cmds.getAttr("defaultResolution.height")
+
+	#get current camera name
+	view = OpenMayaUI.M3dView.active3dView()
+	cam = OpenMaya.MDagPath()
+	view.getCamera(cam)
+	camPath = cam.fullPathName()
+	cmds.select(camPath)
+	currentCamLong = cmds.ls(sl=True,long=False) or []
+	CurrentCam = str(currentCamLong[0])[:-5]
+
+	#check if film gate is on
+	if cmds.camera(CurrentCam, dr=True, q=True):
+		FilmGateOverscan = cmds.camera(CurrentCam, q=True, overscan=1)
+		FilmGateOn = True
+		cmds.camera(CurrentCam, dr=False, e=True)
+		cmds.camera(CurrentCam, e=True, overscan=1.00)
+
+	#check what vers was last
+	CurrentVersCheck = 100
+	while CurrentVersCheck > 0:
+		CurrentVersCheck-=1
+		#    print CurrentVersCheck
+		if cmds.file("images/{}_v{}.jpg".format(CurrentCam, str(CurrentVersCheck).zfill(2)), exists=True, q=True):
+			file = "images/{}_v{}.jpg".format(CurrentCam, str(CurrentVersCheck).zfill(2))
+			print file
+			break
+	CurrentVers = CurrentVersCheck + 1
+	print CurrentVers
+
 	#starts playblast for current frame, as jpeg, at render resolution, without ornaments
-	cmds.playblast(frame=CurrentFrame, format="image", p=100, width=RenderWidth, height=RenderHeight, qlt=95, orn=False)
+	cmds.playblast(
+		frame=CurrentFrame,
+		f="{}_v{}".format(CurrentCam, str(CurrentVers).zfill(2)),
+		fmt="image",
+		p=100,
+		width=RenderWidth,
+		height=RenderHeight,
+		qlt=95,
+		orn=False)
 	#sets render format back to previous
 	cmds.setAttr("defaultRenderGlobals.imageFormat", CurrentImageFormat)
+
+	#rename file to remove ".0000"
+	CurrentProj = cmds.workspace(active=True, q=True)
+	os.chdir("{}".format(CurrentProj))
+	os.rename(
+		"images/{}_v{}".format(CurrentCam, str(CurrentVers).zfill(2)) + ".0000.jpg",
+		"images/{}_v{}".format(CurrentCam, str(CurrentVers).zfill(2)) + ".jpg")
+
+	#turn film gate back on if it was on
+	if FilmGateOn:
+		cmds.camera(CurrentCam, dr=True, e=True)
+		cmds.camera(CurrentCam, e=True, overscan=FilmGateOverscan)
 
 # CREATE WINDOW
 #def main():
