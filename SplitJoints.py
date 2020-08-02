b02937fcd16c93c417ee7e639f4462f61f550d8f
Aury <catanddogsoup@gmail.com>
2020-08-02 17:01:57 +0100

    Script to add intermediate joints between two selected joints and rename them to match joint names

--- SplitJoints.py
+++ SplitJoints.py
@@ -1,15 +1,6 @@
 import maya.cmds as cmds
 
-joints="dab"
-
 joints = cmds.ls(sl=True, type='joint')
-
-#print(joints)[0]
-#print(joints)[1]
-
-#print cmds.xform(joints[0], q=1, ws=1, rp=1)
-#print cmds.xform(joints[1], q=1, ws=1, rp=1)
-
 
 def lerp(min, max, percent):
     return ((max-min)*percent)+min
@@ -31,12 +22,12 @@ cmds.joint(p=pos2)
 cmds.joint(p=pos3)
 cmds.parent(joints[1], cmds.ls(sl=True, type='joint'))
 
-##world orient for joint
-#currentJoint = cmds.ls(sl=True, type='joint')
-#cmds.joint(oj='none', e=True)
-#cmds.select(joints[1])
-
 cmds.rename(cmds.select(cmds.listRelatives(parent=True)), joints[0] + "4")
 cmds.rename(cmds.select(cmds.listRelatives(parent=True)), joints[0] + "3")
 cmds.rename(cmds.select(cmds.listRelatives(parent=True)), joints[0] + "2")
 cmds.rename(joints[0], joints[0] + "1")
+
+##world orient for joint
+#currentJoint = cmds.ls(sl=True, type='joint')
+#cmds.joint(oj='none', e=True)
+#cmds.select(joints[1])
