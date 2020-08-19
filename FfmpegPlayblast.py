106e01703e0d21f1b93d2c2b1bb18c58abb9bab0
aurora <catanddogsoup@gmail.com>
2020-08-19 13:12:17 +0100

    Started on script to export playblasts using ffmpeg

--- FfmpegPlayblast.py
+++ FfmpegPlayblast.py
@@ -1,7 +1,6 @@
 # CREATE WINDOW
-def create_window(self):
-
-    winID = 'aurWindow'
+def create_window():
+    winID = 'playblastFfmpeg'
 
     hv = 25
     wv = 100
@@ -9,7 +8,29 @@ def create_window(self):
     if cmds.window(winID, exists=True):
         cmds.deleteUI(winID)
 
-    cmds.window(winID, title='Control Panel')
-    cmds.columnLayout()
+    cmds.window(winID, title='Playblast Ffmpeg')
+
+    # TODO
+    # ffmpeg location
+    # output location
+    # in/out frames
+    # resolution
+    # format/encoder
+    #
+
+    #cmds.rowLayout(numberOfColumns=3)
+    cmds.columnLayout(w=300)
+    cmds.text(label="Ffmpeg Location")
+    cmds.textField()
+    cmds.setParent('..')
+
+    cmds.columnLayout(w=300)
+    cmds.text(label="Output Location")
+    cmds.textField()
+    cmds.button(label="Export Playblast")
+    cmds.setParent('..')
 
     cmds.showWindow()
+
+
+create_window()
