e0854f663d35ee973b2d96a5c233a083bd07b7a4
catanddogsoup <catanddogsoup@gmail.com>
2020-08-21 23:10:52 +0100

    reformatting and added in more functions

--- FfmpegPlayblast.py
+++ FfmpegPlayblast.py
@@ -1,33 +1,60 @@
+# TODO
+# ffmpeg location /media/aurora/gdrive/Apps/ffmpeg.exe
+# output location
+# // send commands to CLI
+# in/out frames
+# resolution
+# format/encoder
+# actual playblast
+
+# os.system('gnome-terminal && cd ~/Downloads && ffmpeg -i video0.mp4 video1.mp4')
+
+
+
+def printFormatMenuItem(item):
+    video_format = item
+    print video_format
+
+def ffmpeg_playblast(resolutionx, resolutiony, inframe, outframe):
+    print resolutionx
+    print resolutiony
+    print inframe
+    print outframe
+
 # CREATE WINDOW
 def create_window():
     import maya.cmds as cmds
     import os
+
+    if not cmds.optionVar(q='playblastFFmpeg_ffmpegLoc'):
+        cmds.optionVar(sv=('playblastFFmpeg_ffmpegLoc', ''))
+    if not cmds.optionVar(q='playblastFFmpeg_outputLoc'):
+        cmds.optionVar(sv=('playblastFFmpeg_outputLoc', ''))
+    if not cmds.optionVar(q='playblastFFmpeg_outframe'):
+        cmds.optionVar(sv=('playblastFFmpeg_outframe', 100))
+    if not cmds.optionVar(q='playblastFFmpeg_inframe'):
+        cmds.optionVar(sv=('playblastFFmpeg_inframe', 0))
+    if not cmds.optionVar(q='playblastFFmpeg_resolutionx'):
+        cmds.optionVar(sv=('playblastFFmpeg_resolutionx', 1920))
+    if not cmds.optionVar(q='playblastFFmpeg_resolutiony'):
+        cmds.optionVar(sv=('playblastFFmpeg_resolutiony', 1080))
 
     winID = 'playblastFFmpeg'
     if cmds.window(winID, exists=True):
         cmds.deleteUI(winID)
     cmds.window(winID, title='Playblast FFmpeg')
 
-    # TODO
-    # ffmpeg location /media/aurora/gdrive/Apps/ffmpeg.exe
-    # output location
-    # // send commands to CLI
-    # in/out frames
-    # resolution
-    # format/encoder
-    # actual playblast
-
-    # os.system('gnome-terminal && cd ~/Downloads && ffmpeg -i video0.mp4 video1.mp4')
-
     def save_settings(self):
-        cmds.optionVar(sv=('playblastFFmpeg_ffmpegLoc', cmds.textFieldButtonGrp(ffmpegLocationText, q=True, text=True)))
-        cmds.optionVar(sv=('playblastFFmpeg_outputLoc', cmds.textField(outputLocationText, q=True, text=True)))
+        #cmds.optionVar(sv=('playblastFFmpeg_ffmpegLoc', cmds.textFieldButtonGrp(ffmpegLocationText, q=True, text=True)))
+        #cmds.optionVar(sv=('playblastFFmpeg_outputLoc', cmds.textField(outputLocationText, q=True, text=True)))
+        cmds.optionVar(sv=('playblastFFmpeg_outframe', cmds.textField(set_outframe, q=True, text=True)))
+        cmds.optionVar(sv=('playblastFFmpeg_inframe', cmds.textField(set_inframe, q=True, text=True)))
+        cmds.optionVar(sv=('playblastFFmpeg_resolutionx', cmds.textField(set_resolutionx, q=True, text=True)))
+        cmds.optionVar(sv=('playblastFFmpeg_resolutiony', cmds.textField(set_resolutiony, q=True, text=True)))
 
     # FFMPEG LOCATION
     cmds.columnLayout(w=300, rs=5)
-    #cmds.text(label="FFmpeg Location")
     ffmpegLocation = cmds.optionVar(q='playblastFFmpeg_ffmpegLoc')
-    #ffmpegLocationText = cmds.textField(text=ffmpegLocation, ec=save_settings, aie=True, w=300)
     ffmpegLocationText = cmds.textFieldButtonGrp(label='FFmpeg Location', buttonLabel='FFmpeg Location', text=ffmpegLocation, cc=save_settings)
     cmds.button(label="open FFmpeg")
     cmds.setParent('..')
@@ -35,41 +62,55 @@ def create_window():
     # RESOLUTION
     cmds.columnLayout()
     cmds.text(label="Video Resolution")
-    cmds.textField()
-    cmds.textField()
+    resolutionx = cmds.optionVar(q='playblastFFmpeg_resolutionx')
+    set_resolutionx = cmds.textField(text=resolutionx, cc=save_settings)
+    resolutiony = cmds.optionVar(q='playblastFFmpeg_resolutiony')
+    set_resolutiony = cmds.textField(text=resolutiony, cc=save_settings)
 
     # IN/OUT
     cmds.columnLayout()
     cmds.text(label="In and Out Frames")
-    cmds.textField()
-    cmds.textField()
+    inframe = cmds.optionVar(q="playblastFFmpeg_inframe")
+    set_inframe = cmds.textField(text=inframe, cc=save_settings)
+    outframe = cmds.optionVar(q="playblastFFmpeg_outframe")
+    set_outframe = cmds.textField(text=outframe, cc=save_settings)
 
     # FORMAT
-    def printNewMenuItem(item):
-        video_format = item
-        print video_format
-
     cmds.columnLayout()
     cmds.text(label="Video Format")
-    cmds.optionMenu(label='Format', cc=printNewMenuItem)
-    cmds.menuItem(label="h.264")
-    cmds.menuItem(label="h.265")
+    cmds.optionMenu(label='Format', cc=printFormatMenuItem)
+    cmds.menuItem(label="mov h.264")
+    cmds.menuItem(label="mov h.265")
     cmds.menuItem(label="Webm AV9")
 
     # OUTPUT LOCATION
     cmds.columnLayout()
-    #cmds.text(label="Output Location")
     OutputLocation = cmds.optionVar(q='playblastFFmpeg_outputLoc')
-    #outputLocationText = cmds.textField(text=OutputLocation, ec=save_settings, aie=True, w=300)
     outputLocationText = cmds.textFieldButtonGrp(label="Video Output Location", buttonLabel="Set Output Location", text=OutputLocation, cc=save_settings, bc="cmds.fileDialog2(ds=2)")
     cmds.button(label="Output Location")
     cmds.setParent('..')
 
     # EXPORT PLAYBLAST
     cmds.columnLayout()
-    cmds.button(label="Export Playblast", h=50)
+    cmds.button(
+        label="Export Playblast",
+        h=50,
+        c="ffmpeg_playblast(cmds.optionVar(q='playblastFFmpeg_resolutionx'), cmds.optionVar(q='playblastFFmpeg_resolutiony'), cmds.optionVar(q='playblastFFmpeg_inframe'), cmds.optionVar(q='playblastFFmpeg_outframe'))")
 
     cmds.showWindow(winID)
 
 
 create_window()
+
+
+def clearOptionVars():
+    cmds.optionVar(rm='playblastFFmpeg_ffmpegLoc')
+    cmds.optionVar(rm='playblastFFmpeg_outputLoc')
+    cmds.optionVar(rm='playblastFFmpeg_outframe')
+    #print cmds.optionVar(q='playblastFFmpeg_outframe')
+    cmds.optionVar(rm='playblastFFmpeg_inframe')
+    cmds.optionVar(rm='playblastFFmpeg_resolutionx')
+    cmds.optionVar(rm='playblastFFmpeg_resolutiony')
+    #print cmds.optionVar(q='playblastFFmpeg_ffmpegLoc')
+    print "cleared Option Vars"
+#clearOptionVars()
