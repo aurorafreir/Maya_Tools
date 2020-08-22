7810fc5ba6f0fe3005e8135918694709019f3107
catanddogsoup <catanddogsoup@gmail.com>
2020-08-22 22:16:09 +0100

    i think actually finished setting up GUI and text/integer input

--- FfmpegPlayblast.py
+++ FfmpegPlayblast.py
@@ -13,21 +13,23 @@ def printFormatMenuItem(item):
     video_format = item
     print video_format
 
-def ffmpeg_playblast(resolutionx, resolutiony, inframe, outframe):
+def ffmpeg_playblast(resolutionx, resolutiony, inframe, outframe, exportlocation):
     print resolutionx
     print resolutiony
     print inframe
     print outframe
+    cmds.playblast(st=int(inframe), et=int(outframe), w=int(resolutionx), h=int(resolutiony), f=exportlocation)
 
 # CREATE WINDOW
 def create_window():
     import maya.cmds as cmds
+    from functools import partial
     import os
 
     if not cmds.optionVar(q='playblastFFmpeg_ffmpegLoc'):
         cmds.optionVar(sv=('playblastFFmpeg_ffmpegLoc', ''))
     if not cmds.optionVar(q='playblastFFmpeg_outputLoc'):
-        cmds.optionVar(sv=('playblastFFmpeg_outputLoc', ''))
+        cmds.optionVar(sv=('playblastFFmpeg_outputLoc', '/movies/playblast.mov'))
     if not cmds.optionVar(q='playblastFFmpeg_resolutionx'):
         cmds.optionVar(sv=('playblastFFmpeg_resolutionx', 1920))
     if not cmds.optionVar(q='playblastFFmpeg_resolutiony'):
@@ -43,25 +45,31 @@ def create_window():
         cmds.deleteUI(winID)
     cmds.window(winID, title='Playblast FFmpeg')
 
-    def save_settings(self):
-        if cmds.textFieldButtonGrp(ffmpegLocationText, q=True, text=True):
-            cmds.optionVar(sv=('playblastFFmpeg_ffmpegLoc', cmds.textFieldButtonGrp(ffmpegLocationText, q=True, text=True)))
-        if cmds.textField(outputLocationText, q=True, text=True):
-            cmds.optionVar(sv=('playblastFFmpeg_outputLoc', cmds.textField(outputLocationText, q=True, text=True)))
+    def save_settings(*args):
+        if cmds.textFieldButtonGrp('ffmpegLoc', q=True, text=True):
+            cmds.optionVar(sv=('playblastFFmpeg_ffmpegLoc', cmds.textFieldButtonGrp('ffmpegLoc', q=True, text=True)))
+        if cmds.textFieldButtonGrp(outputLocationText, q=True, text=True):
+            cmds.optionVar(sv=('playblastFFmpeg_outputLoc', cmds.textFieldButtonGrp('VideoOutputLoc', q=True, text=True)))
         cmds.optionVar(sv=('playblastFFmpeg_resolutionx', cmds.textField(set_resolutionx, q=True, text=True)))
         cmds.optionVar(sv=('playblastFFmpeg_resolutiony', cmds.textField(set_resolutiony, q=True, text=True)))
-        cmds.optionVar(sv=('playblastFFmpeg_outframe', cmds.textField(set_outframe, q=True, text=True)))
         cmds.optionVar(sv=('playblastFFmpeg_inframe', cmds.textField(set_inframe, q=True, text=True)))
+        cmds.optionVar(sv=('playblastFFmpeg_outframe', cmds.textField(set_outframe, q=True, text=True)))
+
+    def get_ffmpeg_path(*args):
+        file_path = cmds.fileDialog2(fm=1)[0]
+        cmds.textFieldButtonGrp('ffmpegLoc', e=1, text=file_path)
+        save_settings()
 
     # FFMPEG LOCATION
     cmds.columnLayout(w=300, rs=5)
     ffmpegLocation = cmds.optionVar(q='playblastFFmpeg_ffmpegLoc')
-    ffmpegLocationText = cmds.textFieldButtonGrp(label='FFmpeg Location', buttonLabel='FFmpeg Location', text=ffmpegLocation, cc=save_settings)
+    ffmpegLocationText = cmds.textFieldButtonGrp('ffmpegLoc', label='FFmpeg Location', buttonLabel='FFmpeg Location', text=ffmpegLocation, cc=save_settings, bc=get_ffmpeg_path)
     #cmds.button(label="open FFmpeg")
     cmds.setParent('..')
 
     # RESOLUTION
     cmds.columnLayout()
+    cmds.text(label="")
     cmds.text(label="Video Resolution")
     resolutionx = cmds.optionVar(q='playblastFFmpeg_resolutionx')
     set_resolutionx = cmds.textField(text=resolutionx, cc=save_settings)
@@ -70,6 +78,7 @@ def create_window():
 
     # IN/OUT
     cmds.columnLayout()
+    cmds.text(label="")
     cmds.text(label="In and Out Frames")
     inframe = cmds.optionVar(q="playblastFFmpeg_inframe")
     set_inframe = cmds.textField(text=inframe, cc=save_settings)
@@ -78,16 +87,22 @@ def create_window():
 
     # FORMAT
     cmds.columnLayout()
-    cmds.text(label="Video Format")
+    cmds.text(label="")
+    cmds.text(label="")
     cmds.optionMenu(label='Format', cc=printFormatMenuItem)
     cmds.menuItem(label="mov h.264")
     cmds.menuItem(label="mov h.265")
     cmds.menuItem(label="Webm AV9")
 
+    def get_video_output_path(*args):
+        file_path = cmds.fileDialog2()[0]
+        cmds.textFieldButtonGrp('VideoOutputLoc', e=1, text=file_path)
+        save_settings()
+
     # OUTPUT LOCATION
     cmds.columnLayout()
     OutputLocation = cmds.optionVar(q='playblastFFmpeg_outputLoc')
-    outputLocationText = cmds.textFieldButtonGrp(label="Video Output Location", buttonLabel="Set Output Location", text=OutputLocation, cc=save_settings, bc="cmds.fileDialog2(ds=2)")
+    outputLocationText = cmds.textFieldButtonGrp('VideoOutputLoc', label="Video Output Location", buttonLabel="Set Output Location", text=OutputLocation, cc=save_settings, bc=get_video_output_path)
     #cmds.button(label="Output Location")
     cmds.setParent('..')
 
@@ -96,7 +111,7 @@ def create_window():
     cmds.button(
         label="Export Playblast",
         h=50,
-        c="ffmpeg_playblast(cmds.optionVar(q='playblastFFmpeg_resolutionx'), cmds.optionVar(q='playblastFFmpeg_resolutiony'), cmds.optionVar(q='playblastFFmpeg_inframe'), cmds.optionVar(q='playblastFFmpeg_outframe'))")
+        c="ffmpeg_playblast(cmds.optionVar(q='playblastFFmpeg_resolutionx'), cmds.optionVar(q='playblastFFmpeg_resolutiony'), cmds.optionVar(q='playblastFFmpeg_inframe'), cmds.optionVar(q='playblastFFmpeg_outframe'), cmds.optionVar(q='playblastFFmpeg_outputLoc'))")
 
     cmds.showWindow(winID)
 create_window()
