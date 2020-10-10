422004520911abfd9d13664aefcd9b4f712998ad
aurora <catanddogsoup@gmail.com>
2020-10-10 21:41:27 +0100

    Added symmetry option, fixed formatting of variables, added check for if locator already exists, and added autorigger window

--- WorldClickPosition.py
+++ WorldClickPosition.py
@@ -7,68 +7,122 @@ import maya.api.OpenMaya as om
 import maya.api.OpenMayaUI as omui
 import maya.cmds as cmds
 
-global currentJoint
+# global script variables
+global current_joint
+global symmetry
 
+# 2d lerp
 def lerp(min, max, percent):
     return ((max-min)*percent)+min
 
+# 3d lerp
 def vector_lerp(min, max, percent):
     x = lerp(min[0], max[0], percent)
     y = lerp(min[1], max[1], percent)
     z = lerp(min[2], max[2], percent)
     return x, y, z
 
-def rayTrace():
-    vpX, vpY, _ = cmds.draggerContext(rt, query=True, anchorPoint=True)
-    position = om.MPoint()
-    direction = om.MVector()
-    #print vpX, vpY, position, direction
-    omui.M3dView().active3dView().viewToWorld(
-        int(vpX),
-        int(vpY),
-        position,  # world point
-        direction)  # world vector
+def ray_trace():
+    if cmds.objExists('LOC_{}'.format(current_joint)):
+        print "obj already exists"
+        cmds.setToolTo('selectSuperContext')
+    else:
+        vpX, vpY, _ = cmds.draggerContext(rt, query=True, anchorPoint=True)
+        position = om.MPoint()
+        direction = om.MVector()
+        print vpX, vpY, position, direction
+        omui.M3dView().active3dView().viewToWorld(
+            int(vpX), # Viewport click X position as int
+            int(vpY), # Viewport click Y position as int
+            position,  # world point
+            direction)  # world vector
 
-    for mesh in cmds.ls(type='mesh'):
-        # Create a list which can hold MObjects, MPlugs, MDagPaths
-        selectionList = om.MSelectionList()
-        selectionList.add(mesh)  # Add mesh to list
-        dagPath = selectionList.getDagPath(0)  # Path to a DAG node
-        fnMesh = om.MFnMesh(dagPath)  # Function set for operation on meshes
+        for mesh in cmds.ls(type='mesh'):
+            # Create a list which can hold MObjects, MPlugs, MDagPaths
+            selectionList = om.MSelectionList()
+            selectionList.add(mesh)  # Add mesh to list
+            dagPath = selectionList.getDagPath(0)  # Path to a DAG node
+            fnMesh = om.MFnMesh(dagPath)  # Function set for operation on meshes
 
-        # Creates an array of all the worldSpace hits
-        allIntersections = fnMesh.allIntersections(
-            om.MFloatPoint(position),  # raySource
-            om.MFloatVector(direction),  # rayDirection
-            om.MSpace.kWorld,  # space
-            99999,  # maxParam
-            False)  # testBothDirections
-
-    # Extract the different values from the intersection result
-    hitPoint = allIntersections
-
-    # Create a locator at the first and last intersection
-    #cmds.spaceLocator(p=tuple([tup for tup in hitPoint[0][0]][:-1]), n='LOC_firstHit', a=True)
-    #cmds.spaceLocator(p=tuple([tup for tup in hitPoint[0][1]][:-1]), n='LOC_lastHit', a=True)
-
-    #centreLoc = vector_lerp(cmds.xform('LOC_firstHit', q=1, t=1, ws=1), cmds.xform('LOC_lastHit', q=1, t=1, ws=1), .5)
-    #print vector_lerp(cmds.xform('LOC_firstHit', q=1, t=1, ws=1), cmds.xform('LOC_lastHit', q=1, t=1, ws=1), .5)
-    print vector_lerp(tuple([tup for tup in hitPoint[0][0]][:-1]), tuple([tup for tup in hitPoint[0][1]][:-1]), .5)
-    centre = vector_lerp(tuple([tup for tup in hitPoint[0][0]][:-1]), tuple([tup for tup in hitPoint[0][1]][:-1]), .5)
-    cmds.spaceLocator(n='LOC_{}'.format(currentJoint))
-    cmds.xform('LOC_{}'.format(currentJoint), t=centre)
-    print currentJoint
-    cmds.setToolTo('selectSuperContext')
+            # Creates an array of all the worldSpace hits
+            allIntersections = fnMesh.allIntersections(
+                om.MFloatPoint(position),  # raySource
+                om.MFloatVector(direction),  # rayDirection
+                om.MSpace.kWorld,  # space
+                99999,  # maxParam
+                False)  # testBothDirections
+        # Extract hit values from the intersection result
+        hitPoint = allIntersections
+        #sets the 'centre' var to the midpoint first and last intersection
+        centre = vector_lerp(tuple([tup for tup in hitPoint[0][0]][:-1]), tuple([tup for tup in hitPoint[0][1]][:-1]), .5)
+        # creates a locator at 0,0,0, and moves it to the centre point of the click
+        cmds.spaceLocator(n='LOC_{}'.format(current_joint))
+        cmds.xform('LOC_{}'.format(current_joint), t=centre)
+        print ('LOC_{}'.format(current_joint)).replace('_L_', '_R_')
+        if symmetry:
+            current_joint_symmetry = ('LOC_{}'.format(current_joint)).replace('_L_', '_R_')
+            centre_flipped = centre
+            xyz = list(centre)
+            xyz[0] = centre[0] * -1
+            centre_flipped = tuple(xyz)
+            #centre_flipped[0] = centre[0] * -1
+            print centre
+            print centre_flipped
+            cmds.spaceLocator(n=current_joint_symmetry)
+            cmds.xform(current_joint_symmetry, t=centre_flipped)
+            cmds.select('LOC_{}'.format(current_joint))
+        # sets the current tool back to the selector
+        cmds.setToolTo('selectSuperContext')
 
 rt = 'traceDraggerContext'
 
-def worldClickPosition(joint):
-    global currentJoint
-    currentJoint = joint
+def world_click_position(joint):
+    global current_joint
+    current_joint = joint
     # checks if trace dragger context exists, and deletes if it exist
     if cmds.draggerContext(rt, exists=True):
         cmds.deleteUI(rt)
-    cmds.draggerContext(rt, rc=rayTrace, cursor='crossHair')
+    # calls the rayTrace func, and then sets the tool to the rt draggerContext
+    cmds.draggerContext(rt, rc=ray_trace, cursor='crossHair')
     cmds.setToolTo(rt)
 
-worldClickPosition('Shoulder')
+# def build_rig
+
+def create_window():
+    global symmetry
+
+    # sets a window ID
+    winID = 'aurtorigger'
+    # checks if the window ID already exists and deletes it if it does
+    if cmds.window(winID, exists=True):
+        cmds.deleteUI(winID)
+
+    cmds.window(winID, title='Aurtorigger')
+    cmds.columnLayout(adjustableColumn=True, rowSpacing=5, width=200)
+    cmds.checkBox('symmetryCheckbox', label='Automatic Symmetry', v=1, onc='symmetry=1', ofc='symmetry=0')
+    #symmetry = cmds.checkBox('symmetryCheckbox', q=1, v=1)
+
+    cmds.frameLayout(label='Arms', labelAlign='top')
+    cmds.rowColumnLayout("NurbsColours", numberOfColumns=2)
+    cmds.button(label='L_Shoulder', ann='', command='world_click_position("L_Shoulder")')
+    cmds.button(label='R_Shoulder', ann='', command='world_click_position("R_Shoulder")')
+    cmds.setParent('..')
+
+    cmds.frameLayout(label='', labelAlign='top')
+    cmds.button(label='Spine 1', ann='', command='world_click_position("Spine_1")')
+    cmds.button(label='Hips', ann='', command='world_click_position("Hips")')
+    cmds.setParent('..')
+
+    cmds.frameLayout(label='Legs', labelAlign='top')
+    cmds.rowColumnLayout("NurbsColours", numberOfColumns=2)
+    cmds.button(label='L_Leg', ann='', command='world_click_position("L_Leg")')
+    cmds.button(label='R_Leg', ann='', command='world_click_position("R_Leg")')
+    cmds.setParent('..')
+
+    cmds.checkBox(label='')
+
+    cmds.button(label='B U I L D    R I G', bgc=(.8,.3,.3))
+
+    cmds.showWindow()
+
+create_window()
