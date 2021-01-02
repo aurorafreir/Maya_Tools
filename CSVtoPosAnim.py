5c82584582eca3e0be195c90b05af71bc0a043cf
catanddogsoup <catanddogsoup@gmail.com>
2021-01-02 17:25:11 +0000

    load CSV file as translate XYZ position of selected objects

---
+++ CSVtoPosAnim.py
@@ -0,0 +1,17 @@
+import maya.cmds as cmds
+import csv
+
+sel = cmds.ls(sl=1)
+
+results = []
+cur_frame = 0
+with open('/home/aurorafreir/Downloads/300Frames_XYZ_-_Copy.csv') as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+    for row in reader:
+        results.append(row)
+
+for iter, frame in enumerate(results):
+    X, Y, Z = frame[0], frame[1], frame[2]
+    cmds.setKeyframe(sel, v=X, attribute='translateX', t=iter)
+    cmds.setKeyframe(sel, v=Y, attribute='translateY', t=iter)
+    cmds.setKeyframe(sel, v=Z, attribute='translateZ', t=iter)
