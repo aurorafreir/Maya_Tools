f56d46a537595857e85d0e63474e605915e769d2
catanddogsoup <catanddogsoup@gmail.com>
2021-02-11 17:10:56 +0000

    set up class, __init__, and start using vars instead of hardcoded names

--- IKArmWithController.py
+++ IKArmWithController.py
@@ -1,65 +1,84 @@
 import maya.cmds as cmds
 
-# arm_start_location and arm_end_location are shoulder and wrist locations in 3 axis e.g. (0,0,0), (10,0,0)
-# mid_joint_push_back is the amount to push the middle joint backwards (in the Z axis) in Maya units
-# prefix is a prefix before the joint names, e.g. 'L' or 'R'
-def ik_arm(prefix, arm_start_location, arm_end_location, mid_joint_push_back):
-    # Create arm joints
-    cmds.joint(n=prefix+'_Shoulder', p=arm_start_location)
-    cmds.joint(n=prefix+'_Elbow', p=((arm_start_location[0]+arm_end_location[0])/2, 0, 0 - mid_joint_push_back))
-    cmds.joint(n=prefix+'_Wrist', p=arm_end_location)
+class ThreeJointIK(object):
+    def __init__(self, prefix='L', start_location=(0,0,0), end_location=(10,0,0), mid_joint_push_back=1,
+                 joint_one='Shoulder', joint_two='Elbow', joint_three='Wrist'):
+        self.prefix = prefix
+        self.start_location = start_location
+        self.end_location = end_location
+        self.mid_joint_push_back = mid_joint_push_back
+        self.joint_one = joint_one
+        self.joint_two = joint_two
+        self.joint_three = joint_three
+        self.extension = ['GRP', 'CTRL', 'IK']
 
-    # Set Joint Orient so X points down the bone
-    cmds.joint(prefix+'_Shoulder', e=1, zso=1, oj="xyz")
-    cmds.joint(prefix+'_Elbow', e=1, zso=1, oj="xyz")
+    # arm_start_location and arm_end_location are shoulder and wrist locations in 3 axis e.g. (0,0,0), (10,0,0)
+    # mid_joint_push_back is the amount to push the middle joint backwards (in the Z axis) in Maya units
+    # prefix is a prefix before the joint names, e.g. 'L' or 'R'
+    def build(self):
+        # Create arm joints
+        cmds.joint(n=self.prefix + '_' + self.joint_one, p=self.start_location)
+        cmds.joint(n=self.prefix + '_' + self.joint_two,
+                   p=((self.start_location[0]+self.end_location[0])/2, 0, 0 - self.mid_joint_push_back))
+        cmds.joint(n=self.prefix + '_' + self.joint_three, p=self.end_location)
 
-    # Create IK handle from L_Shoulder to L_Wrist
-    cmds.ikHandle(n=prefix+'_Arm_IK', sj=prefix+'_Shoulder', ee=prefix+'_Wrist')
+        # Set Joint Orient so X points down the bone
+        for joint in [self.joint_one, self.joint_two]:
+            cmds.joint(self.prefix + '_' + joint, e=1, zso=1, oj="xyz")
 
-    # Create IK control circle, parent it to empty group, and move to Wrist joint
-    cmds.circle(n=prefix+'_Wrist_IK_CTRL', nrx=1, nrz=0)
-    cmds.group(n=prefix+'_Wrist_IK_GRP', em=1)
-    cmds.parent(prefix+'_Wrist_IK_CTRL', prefix+'_Wrist_IK_GRP')
-    cmds.xform(prefix+'_Wrist_IK_GRP', t=(cmds.xform(prefix+'_Wrist', q=1, t=1, ws=1)))
-    # Parent the IK Handle to the IK Controller
-    cmds.parent(prefix+'_Arm_IK', prefix+'_Wrist_IK_CTRL')
+        # Create IK handle from L_Shoulder to L_Wrist
+        cmds.ikHandle(n=self.prefix+'_Arm_IK', sj=self.prefix + '_' + self.joint_one, ee=self.prefix + '_' + self.joint_three)
 
-    # Create Shoulder controller, parent it to empty group, and move to Shoulder joint
-    cmds.circle(n=prefix + '_Shoulder_CTRL', nrx=1, nrz=0)
-    cmds.group(n=prefix + '_Shoulder_GRP', em=1)
-    cmds.parent(prefix + '_Shoulder_CTRL', prefix + '_Shoulder_GRP')
-    cmds.xform(prefix + '_Shoulder_GRP', t=(cmds.xform(prefix + '_Shoulder', q=1, t=1, ws=1)))
-    # Parent constrain the Shoulder to the Controller
-    cmds.parentConstraint(prefix + '_Shoulder_CTRL', prefix + '_Shoulder')
+        # Create IK control circle, parent it to empty group, and move to Wrist joint
+        cmds.circle(n=self.prefix + '_' + self.joint_three + '_' + self.extension[2] + '_' + self.extension[1], nrx=1, nrz=0)
+        cmds.group(n=self.prefix + '_' + self.joint_three + '_' + self.extension[2] + '_' + self.extension[0], em=1)
+        cmds.parent(self.prefix + '_' + self.joint_three + '_' + self.extension[2] + '_' + self.extension[1],
+                    self.prefix+'_' + self.joint_three + '_' + self.extension[2] + '_' + self.extension[0])
+        cmds.xform(self.prefix + '_' + self.joint_three + '_' + self.extension[2] + '_' + self.extension[0],
+                   t=(cmds.xform(self.prefix+'_Wrist', q=1, t=1, ws=1)))
+        # Parent the IK Handle to the IK Controller
+        #cmds.parent(self.prefix+'_Arm_IK', self.prefix+'_Wrist_IK_CTRL')
+        cmds.parent(self.prefix + '_Arm_' + self.extension[2],
+                    self.prefix + '_' + self.joint_three + '_' + self.extension[2] + '_' + self.extension[1])
 
-    # Create Pole Vector controller that looks visually distinct from the others
-    cmds.circle(n=prefix + '_PV_CTRL', nrx=1, nrz=0)
-    cmds.select(d=1)
-    for x in range(0, 7)[::2]:
-        cmds.select(prefix + '_PV_CTRL' + '.cv[{}]'.format(x), tgl=0, add=True)
-    cmds.selectMode(co=1)
-    cmds.xform(s=(.2, .2, .2))
-    cmds.selectMode(o=1)
-    # Create PV parent group and parent PV control to it, and move behind the elbow location
-    cmds.group(n=prefix + '_PV_GRP', em=1)
-    cmds.parent(prefix + '_PV_CTRL', prefix + '_PV_GRP')
-    cmds.xform(prefix + '_PV_GRP', t=(cmds.xform(prefix + '_Elbow', q=1, t=1, ws=1)))
-    cmds.xform(prefix + '_PV_GRP', t=(0, 0, -mid_joint_push_back*3), r=1)
-    # Parent constrain the Shoulder to the Controller
-    cmds.poleVectorConstraint(prefix + '_PV_CTRL', prefix + '_Arm_IK')
+        # Create Shoulder controller, parent it to empty group, and move to Shoulder joint
+        cmds.circle(n=self.prefix + '_' + self.joint_one+ '_CTRL', nrx=1, nrz=0)
+        cmds.group(n=self.prefix + '_' + self.joint_one+ '_GRP', em=1)
+        cmds.parent(self.prefix + '_' + self.joint_one+ '_CTRL', self.prefix + '_Shoulder_GRP')
+        cmds.xform(self.prefix + '_' + self.joint_one+ '_GRP', t=(cmds.xform(self.prefix + '_Shoulder', q=1, t=1, ws=1)))
+        # Parent constrain the Shoulder to the Controller
+        cmds.parentConstraint(self.prefix + '_Shoulder_CTRL', self.prefix + '_Shoulder')
 
-    # Cleanup
-    # Hide the IK Handle
-    cmds.hide(prefix+'_Arm_IK')
-    # delete history on controller
-    cmds.bakePartialHistory(prefix+'_Wrist_IK_CTRL')
-    # Lock the Scale and View attributes on the controllers
-    cmds.setAttr(prefix+'_Wrist_IK_CTRL.scale', lock=True)
-    cmds.setAttr(prefix+'_Wrist_IK_CTRL.visibility', lock=True)
-    cmds.setAttr(prefix+'_Shoulder_CTRL.scale', lock=True)
-    cmds.setAttr(prefix+'_Shoulder_CTRL.visibility', lock=True)
-    cmds.select(d=1)
+        # Create Pole Vector controller that looks visually distinct from the others
+        cmds.circle(n=self.prefix + '_PV_CTRL', nrx=1, nrz=0)
+        cmds.select(d=1)
+        for x in range(0, 7)[::2]:
+            cmds.select(self.prefix + '_PV_CTRL' + '.cv[{}]'.format(x), tgl=0, add=True)
+        cmds.selectMode(co=1)
+        cmds.xform(s=(.2, .2, .2))
+        cmds.selectMode(o=1)
+        # Create PV parent group and parent PV control to it, and move behind the elbow location
+        cmds.group(n=self.prefix + '_PV_GRP', em=1)
+        cmds.parent(self.prefix + '_PV_CTRL', self.prefix + '_PV_GRP')
+        cmds.xform(self.prefix + '_PV_GRP', t=(cmds.xform(self.prefix + '_Elbow', q=1, t=1, ws=1)))
+        cmds.xform(self.prefix + '_PV_GRP', t=(0, 0, - self.mid_joint_push_back*3), r=1)
+        # Parent constrain the Shoulder to the Controller
+        cmds.poleVectorConstraint(self.prefix + '_PV_CTRL', self.prefix + '_Arm_IK')
+
+        # Cleanup
+        # Hide the IK Handle
+        cmds.hide(self.prefix+'_Arm_IK')
+        # delete history on controller
+        cmds.bakePartialHistory(self.prefix+'_Wrist_IK_CTRL')
+        # Lock the Scale and View attributes on the controllers
+        cmds.setAttr(self.prefix+'_Wrist_IK_CTRL.scale', lock=True)
+        cmds.setAttr(self.prefix+'_Wrist_IK_CTRL.visibility', lock=True)
+        cmds.setAttr(self.prefix+'_Shoulder_CTRL.scale', lock=True)
+        cmds.setAttr(self.prefix+'_Shoulder_CTRL.visibility', lock=True)
+        cmds.select(d=1)
 
 # Run the ik_arm function
-ik_arm('L', (2,0,0), (10,0,0), 1)
+spine = ThreeJointIK(prefix='L', start_location=(2,0,0), end_location=(10,0,0), mid_joint_push_back=1)
+spine.build()
+#three_joint_IK.ik_arm('L', (2,0,0), (10,0,0), 1)
 #ik_arm('R', (-2,0,0), (-10,0,0), 1)
