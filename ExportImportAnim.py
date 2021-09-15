3fd19818dfe16754e36a158a81534baa03c014b7
catanddogsoup <catanddogsoup@gmail.com>
2021-09-15 16:55:14 +0100

    working export/import, not handling key types or tangents yet

--- ExportImportAnim.py
+++ ExportImportAnim.py
@@ -36,6 +36,8 @@ def export_anim():
         if tempname not in controls:
             controls.append(tempname)
 
+    export_data = []
+
     for ctrl in controls:
         ctrlattrs = [ctrl]
         for attr in cmds.listAttr(ctrl, k=1, u=1):
@@ -50,10 +52,13 @@ def export_anim():
                 for time, value in zip(keytimes, keyvalues):
                     keytimevalue.append([time, value])
                 keydata = {ctrl + "." + attr : keytimevalue}
+                
+                export_data.append(keydata)
 
-                with open(jsonfile, "a") as outfile:
-                    json.dump(keydata, outfile, indent=4)
-        
+
+    with open(jsonfile, "w") as outfile:
+        json.dump(export_data, outfile, indent=4)
+
 
 def import_anim(name="", force=False):
     if not name:
@@ -64,9 +69,18 @@ def import_anim(name="", force=False):
 
     jsonfile = projectDirectory + "data/" + name
     print jsonfile
-    with open(jsonfile) as infile:
+    with open(jsonfile, "r") as infile:
         data = json.load(infile)
-    print data
+    for item in data:
+        for ctrlattr in item:
+            for keyframedata in item[ctrlattr]:
+                frame = keyframedata[0]
+                value = keyframedata[1]
+                # inputtangent = keyframedata[2]
+                # outtangent = keyframedata[3]
+                # keytype = keyframedata[4]
+                cmds.setKeyframe(ctrlattr.split(".")[-2], at=ctrlattr.split(".")[-1], t=frame, v=value)
+
 
 
 # Usage in scripts:
