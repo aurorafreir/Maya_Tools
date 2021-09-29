397932dde84848bd3c73b8c79e4de0f4fc1ed0ee
catanddogsoup <catanddogsoup@gmail.com>
2021-09-29 20:10:15 +0100

    cleanup, readability, reformatting, cleanup, readability, reformatti

--- CSVtoPosAnim.py
+++ CSVtoPosAnim.py
@@ -1,20 +1,32 @@
-# Takes an input CSV file and sets it as the Translate X/Y/Z positions on the selected object
-# based on the A/B/C columns in the CSV file, with the rows as frame numbers
+"""
+Takes an input CSV file and sets it as the Translate X/Y/Z positions on the selected object
+based on the A/B/C columns in the CSV file, with the rows as frame numbers
+"""
 
-import maya.cmds as cmds
+# Standard library imports
 import csv
 
-sel = cmds.ls(sl=1)
+# Third party imports
+from maya import cmds
 
-results = []
-cur_frame = 0
-with open('/home/aurorafreir/Downloads/300Frames_XYZ_-_Copy.csv') as csvfile:
-    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-    for row in reader:
-        results.append(row)
+# Local application imports
 
-for iter, frame in enumerate(results):
-    X, Y, Z = frame[0], frame[1], frame[2]
-    cmds.setKeyframe(sel, v=X, attribute='translateX', t=iter)
-    cmds.setKeyframe(sel, v=Y, attribute='translateY', t=iter)
-    cmds.setKeyframe(sel, v=Z, attribute='translateZ', t=iter)
+
+def csvtotransanim(filename=""):
+    sel = cmds.ls(sl=1)
+
+    results = []
+    cur_frame = 0
+
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+        for row in reader:
+            results.append(row)
+
+    # Run through each row, and set the values for each (X, Y, Z) value accordingly
+    for iter, values in enumerate(results):
+        for value, attr in zip(values, ["X", "Y", "Z"]:
+            cmds.setKeyframe(sel, value=value, attribute="translate{}".format(attr), t=iter)
+
+
+csvtotransanim(filename="/home/aurorafreir/Downloads/300Frames_XYZ_-_Copy.csv")
--- ClusterOnCurvePoints.py
+++ ClusterOnCurvePoints.py
@@ -1,8 +1,18 @@
-import maya.cmds as cmds
+"""
+Script to create a cluster for each selected Curve point
+"""
 
-points = cmds.ls(sl=1, fl=1)
-count = 0
-for point in points:
-    count += 1
-    cmds.select(point)
-    cmds.cluster(n=point.split(".")[0] + "_" + str(count))
+# Standard library imports
+
+
+# Third party imports
+from maya import cmds
+
+# Local application imports
+
+
+crvpoints = cmds.ls(selection=1, flatten=1)
+
+for num, crvpoint in enumerate(crvpoints):
+    cmds.select(crvpoint)
+    cmds.cluster(n="{}_{}".format(crvpoint.split(".")[0], str(num)))
