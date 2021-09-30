dbce22a4694e414fd8279d81d153a0b20c5807f2
pilou <pierre.violanti@gmail.com>
2021-09-29 20:52:35 -0400

    update repo

--- python/auroraLib/__init__.py
+++ python/auroraLib/__init__.py
@@ -1,3 +1,7 @@
 
 
-# this init file is created to convert your
+# this __init__ file is created to convert your directory to a python package
+# doing so allows you now to do
+
+# import auroraLib.createNurbsShapes
+# auroraLib.createNurbsShapes.create_nurbs_circle()
