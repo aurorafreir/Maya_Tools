b78131bcc88962efea3ee316a2396fb9a0c540c8
catanddogsoup <catanddogsoup@gmail.com>
2021-09-30 21:36:56 +0100

    deletion/moving of a load of files

--- GroupEachSeperately.py
+++
@@ -1,9 +0,0 @@
-# Set as Ctrl+Shift+G
-
-import maya.cmds as cmds
-
-sel = cmds.ls(sl=1)
-
-for i in sel:
-    cmds.select(i)
-    cmds.group(n='GRP_{}'.format(i).replace('JNT_',''))
--- RenderEndFrameAsTimeline.py
+++
@@ -1,4 +0,0 @@
-import maya.cmds as cmds
-EndFrame = cmds.playbackOptions(q=True, maxTime=1)
-cmds.setAttr('defaultRenderGlobals.endFrame', EndFrame)
-print ("set Render Range end frame to " + str(EndFrame))
