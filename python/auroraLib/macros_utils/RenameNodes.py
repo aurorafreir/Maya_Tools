bc59ea7837bd69cec0e6061da04788af7f36354c
catanddogsoup <catanddogsoup@gmail.com>
2021-10-02 00:35:38 +0100

    Formatting docstring, import libs, naming conventions, etc

--- python/auroraLib/macros_utils/RenameNodes.py
+++ python/auroraLib/macros_utils/RenameNodes.py
@@ -1,11 +1,22 @@
-import maya.cmds as cmds
+"""
+Script used for renaming different types of objects inside of the current Maya scene
+"""
+
+# Standard library imports
+
+# Third party imports
+from maya import cmds
+
+# Local application imports
+
+
 
 def renameSkinClusters():
-    for SC in cmds.ls(type="skinCluster"):
-        SCobject = cmds.listConnections(SC + ".outputGeometry[0]")[0]
-        cmds.rename(SC, SCobject + "_SkinCluster")
+    for skincluster in cmds.ls(type="skinCluster"):
+        sc_object = cmds.listConnections("{}.outputGeometry[0]".format(skincluster))[0]
+        cmds.rename(skincluster, "{}_SkinCluster".format(sc_object))
 
 def renameUnitConversionNodes():
-    for i in cmds.ls(type="unitConversion"):
-        input = cmds.listConnections(i)[0]
-        cmds.rename(i, input + "_UC")
+    for unitconversion in cmds.ls(type="unitConversion"):
+        input = cmds.listConnections(unitconversion)[0]
+        cmds.rename(unitconversion, "{}_UC".format(input))
