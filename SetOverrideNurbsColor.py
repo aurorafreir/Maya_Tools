6cb083a71730191a236c93838611c06e7541a3b4
catanddogsoup <catanddogsoup@gmail.com>
2021-10-01 15:03:22 +0100

    refactor - cleanup of file directories

--- SetOverrideNurbsColor.py
+++
@@ -1,9 +0,0 @@
-ctrl = cmds.ls(sl=True)
-for i in ctrl:
-    cmds.setAttr(i + ".overrideEnabled",1)
-    cmds.setAttr(i + ".overrideColor", 18)
-
-ctrl = cmds.ls(sl=True)
-for i in ctrl:
-    cmds.setAttr(i + ".overrideEnabled",1)
-    cmds.setAttr(i + ".overrideColor", 17)
