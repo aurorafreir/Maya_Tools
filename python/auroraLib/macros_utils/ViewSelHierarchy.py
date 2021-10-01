0b5d3c59ab1c46b11943cecac196758878e4a4e3
catanddogsoup <catanddogsoup@gmail.com>
2021-10-02 00:55:25 +0100

    refactor, init files for each module, styling

--- python/auroraLib/macros_utils/ViewSelHierarchy.py
+++ python/auroraLib/macros_utils/ViewSelHierarchy.py
@@ -1,16 +1,29 @@
+"""
+Script used for exporting and importing all the controller animation in a scene
+"""
+
+# Standard library imports
+
+
+# Third party imports
 from maya import cmds
-import maya.mel as mel
+from maya import mel
+
+# Local application imports
+
+
 
 def outliner_focus():
-    outliner = [i for i in cmds.lsUI(ed=1) if 'outliner' in i]
+    outliner = [i for i in cmds.lsUI(editors=1) if 'outliner' in i]
     if outliner:
         outliner = outliner[0]
     else:
         return
 
-    cmds.outlinerEditor(outliner, e=1, sc=1)
+    cmds.outlinerEditor(outliner, edit=1, showSelected=1)
     cmds.setFocus(outliner)
 
 outliner_focus()
-cmds.select(hi=1)
+cmds.select(hierarchy=1)
 mel.eval('OutlinerRevealSelected;')
+
