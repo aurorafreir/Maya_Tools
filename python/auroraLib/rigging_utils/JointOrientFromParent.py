b360aa860aa4e1db70a603d352b192e2e6d9b37f
catanddogsoup <catanddogsoup@gmail.com>
2021-10-02 15:06:21 +0100

    formatting for script, imports, variables, and flag names

--- python/auroraLib/rigging_utils/JointOrientFromParent.py
+++ python/auroraLib/rigging_utils/JointOrientFromParent.py
@@ -1,16 +1,28 @@
-import maya.cmds as cmds
+"""
+Script to make a new joint beneath the selection, placed outwards from the parent joint
+Used to help set the Joint Orient for any leaf joints
+"""
+
+# Standard library imports
+
+# Third party imports
+from maya import cmds
+
+# Local application imports
+
 
 def JointOrientFromParent():
-    selected_joints = cmds.ls(sl=1, type="joint")
+    selected_joints = cmds.ls(selection=True, type="joint")
 
     for joint in selected_joints:
-        if not joint[-4:] == "_end":
-            parent_joint = cmds.listRelatives(joint, p=1, f=1)[0]
-            cmds.duplicate(joint, n=joint + "_end")
-            cmds.aimConstraint(parent_joint, joint+"_end")
-            cmds.xform(joint+"_end", r=1, os=1, t=(-.5,0,0))
-            cmds.parent(joint+"_end", joint)
-            cmds.delete(joint + "_end_aimConstraint1")
-            cmds.xform(joint + "_end", ro=(0,0,0))
+        end_joint_name = "{}_end".format(joint)
+        if end_joint_name not in joint:
+            parent_joint = cmds.listRelatives(joint, parent=True, fullPath=True)[0]
+            cmds.duplicate(joint, name=end_joint_name)
+            cmds.aimConstraint(parent_joint, end_joint_name)
+            cmds.xform(end_joint_name, relative=True, objectSpace=True, translation=(-.5,0,0))
+            cmds.parent(end_joint_name, joint)
+            cmds.delete("{}_aimConstraint1".format(end_joint_name))
+            cmds.xform(end_joint_name, rotation=(0,0,0))
 
 JointOrientFromParent()
