ba60bcbed184debbadb0efe53c2b8feed65f0681
CatAndDogSoup <CatAndDogSoup@gmail.com>
2022-11-05 16:38:18 +0000

    Updating runner script to have correct shelf env variable

--- maya_runner.sh
+++ maya_runner.sh
@@ -6,6 +6,6 @@ export PYTHONPATH="$SCRIPT_DIR":
 export MAYA_MODULE_PATH="$SCRIPT_DIR/mgear_4.10.0/Release"
 export MAYA_PLUG_IN_PATH="$SCRIPT_DIR/plugins"
 export XBMLANGPATH="$SCRIPT_DIR/icons"
-export MAYA_SHELVES_PATH="$SCRIPT_DIR/shelves"
+export MAYA_SHELF_PATH="$SCRIPT_DIR/shelves"
 
 maya
