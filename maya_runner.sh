908bdd6e1c6f38396faca188513e1d77b73c86a3
aurorafreir <CatAndDogSoup@gmail.com>
2022-11-12 11:48:22 +0000

    Including mgear shifter path as default now

--- maya_runner.sh
+++ maya_runner.sh
@@ -3,9 +3,13 @@
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) # Gets this script's running directory
 
 export PYTHONPATH="$SCRIPT_DIR":
-export MAYA_MODULE_PATH="$SCRIPT_DIR/mgear_4.10.0/Release"
+export MAYA_MODULE_PATH="$SCRIPT_DIR/mgear_4.0.9/release"
 export MAYA_PLUG_IN_PATH="$SCRIPT_DIR/plugins"
-export XBMLANGPATH="$SCRIPT_DIR/icons"
+export XBMLANGPATH="$SCRIPT_DIR/icons/%B"
 export MAYA_SHELF_PATH="$SCRIPT_DIR/shelves"
+
+export MGEAR_SHIFTER_COMPONENT_PATH="$SCRIPT_DIR/mGearScripts/Components/"
+
+#export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libffi.so.6"
 
 maya
